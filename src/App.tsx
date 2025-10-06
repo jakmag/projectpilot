70f5f48716d0aa95a352e753176642782a886ea4
jakmag <magnes@brightpick.ai>
2025-10-06 17:07:05 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import ProjectsPage from "./projects/ProjectsPage";
+import Hello from "./projects/Hello";
+
+
+function App() {
+  return (
+    <div className="container">
+      <Hello name="test name" enthusiasmLevel={0} />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/projects/Project.ts
@@ -0,0 +1,39 @@
+interface ProjectData {
+  id: number;
+  name: string;
+  description: string;
+  imageUrl: string;
+  contractTypeId: number;
+  contractSignedOn: Date | string; // Allow string for raw data
+  budget: number;
+  isActive: boolean;
+}
+
+
+export class Project {
+  id: number | undefined;
+  name: string = '';
+  description: string = '';
+  imageUrl: string = '';
+  contractTypeId: number | undefined;
+  contractSignedOn: Date = new Date();
+  budget: number = 0;
+  isActive: boolean = false;
+  get isNew(): boolean {
+    return this.id === undefined;
+  }
+
+  constructor(initializer?: Partial<ProjectData>) {
+    if (!initializer) return;
+    if (initializer.id) this.id = initializer.id;
+    if (initializer.name) this.name = initializer.name;
+    if (initializer.description) this.description = initializer.description;
+    if (initializer.imageUrl) this.imageUrl = initializer.imageUrl;
+    if (initializer.contractTypeId)
+      this.contractTypeId = initializer.contractTypeId;
+    if (initializer.contractSignedOn)
+      this.contractSignedOn = new Date(initializer.contractSignedOn);
+    if (initializer.budget) this.budget = initializer.budget;
+    if (initializer.isActive) this.isActive = initializer.isActive;
+  }
+}
